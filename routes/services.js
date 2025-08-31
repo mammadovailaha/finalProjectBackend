4e8e67c79bf997082557a68ae826520532b3489b
mammadovailaha <ilamemmedova14@gmail.com>
2025-08-31 13:38:55 +0400

    router server

--- routes/services.js
+++ routes/services.js
@@ -1,28 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
 const {
   getAllServices,
   createService,
   updateService,
-  deleteService,
-} = require("../controllers/servicesController");
+  deleteService
+} = require("../controllers/servicesController"); // controller faylından
 
-// Bütün servisləri gətir
+// Routes
 router.get("/", getAllServices);
-
-// Yeni servis yarat
 router.post("/", createService);
-
-// Servisi ID-ə görə yenilə
 router.put("/:id", updateService);
-
-// Servisi ID-ə görə sil
 router.delete("/:id", deleteService);
-console.log("📋 Controller funksiyaları:", {
-  getAllServices: typeof getAllServices,
-  createService: typeof createService,
-  updateService: typeof updateService,
-  deleteService: typeof deleteService
-});
+
 module.exports = router;
